f7ef01afdd6d23245ef2517872cab62208d44b68
gurubhaskary <syguru82sun@gmail.com>
2022-09-14 16:30:27 +0530

    api1

--- src/Model/collegeModel.js
+++ src/Model/collegeModel.js
@@ -11,3 +11,5 @@ const collegeModel = new mongoose.Schema(
     }, { timestamps: true })
 
 module.exports = mongoose.model('CollegeData', collegeModel)
+
+//https://functionup-stg.s3.ap-south-1.amazonaws.com/thorium/iitd.png
